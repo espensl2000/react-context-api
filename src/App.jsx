7bad32d8090648f1b5c7b1cd70de064d7d908ac8
Espen Luna <espensluna@gmail.com>
2024-10-01 12:20:02 +0200

    Part 1 & part 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useContext, createContext } from 'react'
 import Header from './components/Header'
 import Tweets from './components/Tweets'
 import RightSide from './components/RightSide'
 import defaultTweets from './assets/data/tweets.js'
 import user from './assets/data/user.js'
 
+export const UserContext = createContext()
+export const ThemeContext = createContext()
+
 function App() {
     const [tweets, setTweets] = useState(defaultTweets)
-    const [theme, setTheme] = useState('light');
-
+    const [theme, setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light');
+    
     useEffect(() => {
         theme === 'light'
           ? document.body.style.backgroundColor = 'white'
@@ -17,9 +20,19 @@ function App() {
 
     return (
         <div className="container">
-            <Header user={user} theme={theme} setTheme={setTheme} />
-            <Tweets tweets={tweets} setTweets={setTweets} user={user} theme={theme}  />
-            <RightSide theme={theme} />
+        <UserContext.Provider
+            value={{user: user, tweets: tweets, setTweets: setTweets}}
+        >
+            <ThemeContext.Provider
+                value={{theme: theme, setTheme: setTheme}}
+            >
+                <Header/>
+                <Tweets/>
+                <RightSide/>
+            </ThemeContext.Provider>
+
+        </UserContext.Provider>
+
         </div>
     )
 }
--- src/components/Tweets.jsx
+++ src/components/Tweets.jsx
@@ -1,7 +1,13 @@
+import { useContext } from 'react'
 import CreateTweet from './CreateTweet'
 import Tweet from './Tweet'
+import { ThemeContext, UserContext } from '../App'
 
-export default function Tweets({ tweets, setTweets, user, theme }) {
+export default function Tweets() {
+
+    const { tweets, setTweets, user } = useContext(UserContext)
+    const { theme } = useContext(ThemeContext)
+
   return (
         <main>
             <div className={theme === 'dark' ? 'top-bar dark' : 'top-bar'}>
